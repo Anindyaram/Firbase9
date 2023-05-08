ccf90b08242b63321868aeeed38242080c5f8b17
Anindya Tripathi <64327113+Anindyaram@users.noreply.github.com>
2023-05-09 01:15:16 +0530

    Connected firebase and added webpack

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCZDr2_R9l8bAtIpqy2ATKVQFdCjv19v54",
+  authDomain: "my-first-project-d6b7f.firebaseapp.com",
+  projectId: "my-first-project-d6b7f",
+  storageBucket: "my-first-project-d6b7f.appspot.com",
+  messagingSenderId: "927866744271",
+  appId: "1:927866744271:web:0d404d1d10554b36b076ba",
+};
+
+initializeApp(firebaseConfig);
